e543ad152dca2f83fd683d1eda3d9847f6ac3a69
PikaKight <tmmarcus73@gmail.com>
2022-01-07 22:23:17 -0500

    finished the 30 min react tutorial

---
+++ React/in-30min/src/App.js
@@ -0,0 +1,55 @@
+import React, {useState, useRef, useEffect} from 'react';
+import Todolist from './Todolist';
+import {v4 as uuidv4} from 'uuid'
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
+
+function App() {
+    const [todos, setTodos] = useState([])
+    const todoNameRef = useRef()
+
+    useEffect(() => {
+      const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      if (storedTodos) setTodos(storedTodos)
+    }, [])
+
+    useEffect(() => {
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    }, [todos])
+
+    function toogleTodo(id){
+      const newTodos = [...todos]
+      const todo = newTodos.find(todo => todo.id === id)
+      todo.complete = !todo.complete
+      setTodos(newTodos)
+    }
+
+    function handleAddTodo(e){
+        const name = todoNameRef.current.value
+        if (name === '') return
+        setTodos(prevTodos => {
+          return [...prevTodos, {id:uuidv4(), name:name, cmplete:false}]
+        })
+        todoNameRef.current.value = null
+    }
+
+    function clearComplete(e){
+      const newTodos = todos.filter(todo => !todo.complete)
+      setTodos(newTodos)
+    }
+
+    return ( 
+      <>
+        <Todolist todos={todos} toggleTodo = {toogleTodo}/>
+        <input ref={todoNameRef} type="Text"/>
+        <button onClick={handleAddTodo}>  Add Todo</button>
+        <button onClick={clearComplete}>Clear Complete</button>
+        <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+      </>
+      
+    )
+}
+
+
+
+export default App;
